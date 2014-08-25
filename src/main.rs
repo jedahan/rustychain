4badf5f839a09d2b5dee877f7fea6f5d8637942e
Jonathan Dahan <jonathan@jedahan.com>
2014-08-25 16:01:26 -0400

    fix hashing, thanks nham

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ extern crate crypto = "rust-crypto";
 use std::collections::HashSet;
 
 use core::cmp::{Eq,PartialEq};
-use std::hash::Hash;
+use std::hash::{Hash,hash};
 use std::hash::sip::SipState;
 
 use crypto::sha2::Sha256;
@@ -19,84 +19,63 @@ struct TransactionHeader {
   to: User,
   time: time::Timespec,
   payload_hash: Vec<u8>,
-  payload_length: Vec<u8>,
+  payload_length: uint,
   nonce: int
 }
 
 impl Hash for TransactionHeader {
 
   fn hash(&self, state: &mut SipState) {
-
-    let mut digest = Sha256::new();
-
-      let transactionHeader = format!("{}{}{}{}{}{}{}",
-        self.from,
-        self.to,
-        self.time.sec,
-        self.time.nsec,
-        self.payload_hash,
-        self.payload_length,
-        self.nonce);
-
-    digest.input(transactionHeader.as_bytes());
-    digest.result_str();
+    self.from.hash(state);
+    self.to.hash(state);
+    self.time.sec.hash(state);
+    self.time.nsec.hash(state);
+    self.payload_hash.hash(state);
+    self.payload_length.hash(state);
+    self.nonce.hash(state);
   }
 }
 
-#[deriving(Eq,PartialEq,Show)]
+#[deriving(Eq,PartialEq,Show,Hash)]
 struct Transaction {
   header: TransactionHeader,
   payload: Vec<u8>,
   signature: Vec<u8>
 }
 
-impl Hash for Transaction {
-
-  fn hash(&self, state: &mut SipState) {
-
-    let mut digest = Sha256::new();
-
-    let transaction = format!("{}{}",
-      self.header,
-      self.payload);
-
-    digest.input(transaction.as_bytes());
-    digest.result_str();
-  }
-}
-
-
-fn mutateTransactionHeaderUntilProofOfWorkIsOk(header: TransactionHeader, complexity: uint) {
+fn mutate_transaction_header_until_proof_of_work_is_ok(header: &mut TransactionHeader, complexity: uint) {
   let mut try = 0i;
 
   loop {
       try += 1;
       header.nonce = try;
 
-      let s = String::from_char(complexity*2,'0');
-      if header.hash().as_bytes().slice(0,complexity*2) == s.as_bytes().slice(0,complexity*2) {
-        println!("we found {} after {} tries", header.hash(), try);
+      // check that the first *complexity* bytes are 0
+      if hash(header) < (1 << (64-(8*complexity))) {
+        println!("we found {} after {} tries", hash(header), try);
         return;
       }
   }
 }
 
 
-fn makeTransaction(from: User, to: User, payload: Vec<u8>) -> Transaction {
-  let mut digest = Sha256::new();
+fn makeTransaction(from: User, to: User, payload: Vec<u8>) -> TransactionHeader {
+  /*let mut digest = Sha256::new();
   digest.input(payload);
-
+*/
   let mut header = TransactionHeader {
       from: from,
       to: to,
       time: time::get_time(),
-      payload_hash: digest.result_str(),
+      payload_hash: vec![0],
       payload_length: payload.len(),
       nonce: 0
   };
 
-  mutateTransactionHeaderUntilProofOfWorkIsOk(header, 2);
+  mutate_transaction_header_until_proof_of_work_is_ok(&mut header, 3);
 
+  header
+/*
   let signature = sign(header.hash());
 
   Transaction {
@@ -104,12 +83,13 @@ fn makeTransaction(from: User, to: User, payload: Vec<u8>) -> Transaction {
     payload: payload,
     signature: signature
   }
+  */
 }
 
 fn main() {
   let mut ledger = HashSet::new();
 
-  let first_transaction = makeTransaction("Alice","Bob","Red Stamp");
+  let first_transaction = makeTransaction("Alice","Bob",Vec::from_slice(b"Red Stamp"));
   ledger.insert(first_transaction);
 
   for entry in ledger.iter() {
